cdb1dd9fca5929928875da8216870da8bf8df790
bhyago <bhyago@gmail.com>
2020-01-29 13:54:09 -0500

    middlewares de autenticação

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+
+export default (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  return next();
+};
