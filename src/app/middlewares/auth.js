52f98fb09329a4123f0cbf7196c040a047f7921a
bhyago <bhyago@gmail.com>
2020-01-29 14:08:24 -0500

    controller de update do usuario

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,15 +1,25 @@
 import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  // eslint-disable-next-line no-console
   if (!authHeader) {
-    return res.status({ error: 'Token not provided' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
 
-  return next();
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
 };
